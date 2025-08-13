c9f44ab36a7fe3f57e7e5b8a601424cc094c8e2f
Deepak Kumar Yadav <136052399+Deepak9162@users.noreply.github.com>
2025-08-13 07:26:59 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
+app.use(cors({
+  origin: ["https://demo090.netlify.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.get("/", (req, res) => {
   res.send("Hello from Backend!");
 });
@@ -12,3 +18,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
